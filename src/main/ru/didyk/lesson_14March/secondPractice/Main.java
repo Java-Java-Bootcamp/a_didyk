a2ee407f453fbc0567ae3d8fa359418be2159401
Aleksey Didyk <alekseydidyk@yandex.ru>
2022-03-15 09:13:31 +0300

    14 March

--- src/main/ru/didyk/lesson_14March/secondPractice/Main.java
+++ src/main/ru/didyk/lesson_14March/secondPractice/Main.java
@@ -11,14 +11,28 @@ public class Main {
         Student student1 = new Student("Alex D", "prof", 30);
         Student student2 = new Student("Waks D", "prof", 30);
         Student student3 = new Student("Alexs D", "prof", 30);
+        Student student4 = new Student("Alex D", "prof", 30);
 
         students.add(student1);
         students.add(student2);
         students.add(student3);
+        students.add(student4);
 
-//        System.out.println(students);
-        System.out.println(getN(students, "Alex", "Alexs"));
+//        System.out.println(getN(students, "Alex", "Alexs"));
+        Map<String, Integer> t = getT(students);
+        for (Map.Entry<String, Integer> entry : t.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue().toString());
+        }
 
+    }
+
+    public static Map<String, Integer> getT(List<Student> students) {
+//        return students.stream().collect(Collectors.groupingBy(s -> s.getFullName()));
+        return students.stream().collect(Collectors.toMap(
+                s -> s.getSpeciality(),
+                s -> 1,
+                Integer::sum
+        ));
     }
 
     public static Double getGpa(List<Student> students) {
@@ -40,7 +54,7 @@ public class Main {
     public static List<Student> getN(List<Student> students, String name, String name1) {
         return students.stream()
                 .filter(s -> s.getFullName().split(" ")[0].equals(name))
-                .collect(Collectors.toList());
+                .toList();
     }
 
 //    public static Map<String, List<Student>> getSpeciality(List<Student> students) {
